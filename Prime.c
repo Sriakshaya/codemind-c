c3023e91dde981b590408f50aa8bc6490747f387
Sriakshaya <119840507+Sriakshaya@users.noreply.github.com>
2022-12-09 21:23:53 +0530

    #include<stdio.h> int pr(int n,int i,int fc ); int main() {     int n,i,fc=0;     scanf("%d",&n);     int x=pr(n,i,fc); } int pr(int n,int i,int fc ) {     for(i=1;i<=n;i++)     {         if (n%i==0)         {             fc=fc+1;         }     }     if (fc==2)     {         printf("Prime");     }     else     {         printf("Not Prime");     }     return 0; }

--- Prime.c
+++ Prime.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int pr(int n,int i,int fc );
 int main()
 {
     int n,i,fc=0;
     scanf("%d",&n);
+    int x=pr(n,i,fc);
+}
+int pr(int n,int i,int fc )
+{
     for(i=1;i<=n;i++)
     {
         if (n%i==0)
@@ -18,4 +23,5 @@ int main()
     {
         printf("Not Prime");
     }
+    return 0;
 }
